6347f128c9e9c7c0c5b1c4dff65905002f6bdf08
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-02-26 10:42:16 +0530

    [ADDED]:Ritesh - This Code Show Wellcome To Line Comaparison Problem  Massege

---
+++ src/com/ritesh/linecomp/LineCompProblem.java
@@ -0,0 +1,11 @@
+package com.ritesh.linecomp;
+
+public class LineCompProblem {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Wellcome to Line Comapison Computation Program");
+
+	}
+
+}
