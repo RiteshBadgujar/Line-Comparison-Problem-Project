d1e73f3f513140818842d1bbe53b68c68ef3b7cd
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-03-04 15:22:54 +0530

    [MODIFIED]:Ritesh UC-1 Check For Line Length

--- src/com/ritesh/linecomp/LineCompProblem.java
+++ src/com/ritesh/linecomp/LineCompProblem.java
@@ -5,29 +5,25 @@ import java.util.Scanner;
 public class LineCompProblem {
 
 	public static void main(String[] args) {
-		int x1=0;
-		int x2=6;
-		int y1=0;
-		int y2=6;
+		int x1;
+		int x2;
+		int y1;
+		int y2;
 	    double length;
-		 
+
 		Scanner scanner =new Scanner(System.in); 
-		
+
 		System.out.println("Wellcome To Line Comapison Computation Program");
 		System.out.println("Enter The Number X1,Y1 And X2,Y2 Point For The Line : ");
-		
+
 		x1=scanner.nextInt();
 		x2=scanner.nextInt();
 		y1=scanner.nextInt();
 		y2=scanner.nextInt();
-		
+
 		length=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-		 
-		 System.out.println("Length Line :"+length);		 	 
-		 
+
+	    System.out.println("Length Line :"+length);		 	 
+
 	}
-
-	
 }
-
-
